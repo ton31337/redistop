743800930bc1085ae1e72084463c3cb67b8f34fc
Donatas Abraitis <donatas.abraitis@gmail.com>
2015-08-25 19:52:59 +0300

    Usage and license

--- redistop.rb
+++ redistop.rb
@@ -1,6 +1,27 @@
 #!/opt/rbenv/shims/ruby
-# prints mostly used functions and counters
-# @ton31337 <donatas@vinted.com>
+
+=begin
+redistop.rb - show redis utilization
+Usage: rubytop.rb [options]
+    -e, --exclude <string>           Exclude cmd
+    -n, --num <integer>              Print only X commands
+    -r, --refresh <integer>          Refresh interval
+    -s, --sort_time                  Sort by time
+    -h, --help                       Displays Help
+
+Copyright (C) 2015 Donatas Abraitis <donatas@vinted.com>.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
 
 require 'optparse'
 
